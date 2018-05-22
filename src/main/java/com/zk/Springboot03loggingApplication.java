c17112444e7f1a82d243645312abe0149dfb5f41
赵锟 <zhaokun@bonc.com.cn>
2018-05-23 00:14:10 +0800

    Initial commit

---
+++ src/main/java/com/zk/Springboot03loggingApplication.java
@@ -0,0 +1,12 @@
+package com.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03loggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03loggingApplication.class, args);
+	}
+}
