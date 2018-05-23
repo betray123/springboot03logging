bf660663cabb03b91b3d22da3970687378e63251
赵锟 <zhaokun@bonc.com.cn>
2018-05-23 23:24:30 +0800

    test log

--- src/test/java/com/zk/Springboot03loggingApplicationTests.java
+++ src/test/java/com/zk/Springboot03loggingApplicationTests.java
@@ -2,6 +2,8 @@ package com.zk;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Springboot03loggingApplicationTests {
 
+	//记录器
+	Logger logger = LoggerFactory.getLogger(getClass());
 	@Test
 	public void contextLoads() {
+
+		//日志级别:
+		//由低到高依次是:trace<debug<info<warn<error
+		//可以调整输出日志级别;日志就只会以在这个级别以后的高级别生效
+		logger.trace("这是trace日志");
+		logger.debug("这是debug日志");
+		//springboot默认给我们配置使用的是info级别及以上的日志,配置文件没指定logger级别,就使用springboot默认的root级别.
+		logger.info("这是info日志");
+		logger.warn("这是warn日志");
+		logger.error("这是error日志");
 	}
 
 }
